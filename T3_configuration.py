c2bcc90c9141ad96f05813dca5846c66f179c028
Andrea Mendizabal <andrea.mendizabal.echeverria@gmail.com>
2022-05-12 17:15:47 +0200

    BA-Net training with DPX

---
+++ T3_configuration.py
@@ -0,0 +1,52 @@
+"""
+#03 - Configurations
+Define configurations for Environment, Network and Dataset
+"""
+
+# Python imports
+import torch
+
+# DeepPhysX related imports
+from DeepPhysX_Core.Environment.BaseEnvironmentConfig import BaseEnvironmentConfig
+# from DeepPhysX_Core.Network.BaseNetworkConfig import BaseNetworkConfig
+from DeepPhysX_Core.Dataset.BaseDatasetConfig import BaseDatasetConfig
+from DeepPhysX_PyTorch.UNet.UNetConfig import UNetConfig
+
+
+# Tutorial related imports
+from T1_environment import BanetEnvironment
+from T2_network import BanetNetwork, BanetOptimization
+
+
+# Create the Environment config
+env_config = BaseEnvironmentConfig(environment_class=BanetEnvironment,      # The Environment class to create
+                                   visualizer=None,                         # The Visualizer to use
+                                   simulations_per_step=1,                  # The number of sub-steps to run
+                                   use_dataset_in_environment=False,        # Dataset will not be sent to Environment
+                                   param_dict={'increment': 1},             # Parameters to send at init
+                                   as_tcp_ip_client=True,                   # Create a Client / Server architecture
+                                   number_of_thread=1,                      # Number of Clients connected to Server
+                                   ip_address='localhost',                  # IP address to use for communication
+                                   port=10001)
+
+# Create the Network config
+net_config = UNetConfig(loss=torch.nn.MSELoss,
+                        lr=0.001,
+                        optimizer=torch.optim.Adam,
+                        network_name='BanetNetwork',
+                        nb_dims=3,
+                        input_size=[27, 27, 32],
+                        nb_input_channels=4,
+                        nb_first_layer_channels=64,
+                        nb_output_channels=1,
+                        nb_steps=3,
+                        two_sublayers=True,
+                        border_mode='same',
+                        # skip_merge=True,
+                        save_each_epoch=True,
+                        data_scale=1.0)
+
+
+# Create the Dataset config
+dataset_config = BaseDatasetConfig(partition_size=3, shuffle_dataset=True)
+
---
+++ convert_vts_to_npy.py
@@ -0,0 +1,74 @@
+import os
+import numpy as np
+from vtk import *
+from vtk.util import numpy_support
+
+####### VTK functions ##################################################################################################
+def readGrid( grid_filename ):
+	reader = vtkXMLStructuredGridReader()
+	reader.SetFileName(grid_filename)
+	reader.Update()
+	return reader.GetOutput()
+
+
+def getDataArray( grid_vtk, field_name ):
+    # Get data array and convert it to numpy
+    if not grid_vtk.GetPointData().HasArray(field_name):
+        raise IOError("Grid {} does not contain the {} array".format(grid_vtk, field_name))
+    return numpy_support.vtk_to_numpy( grid_vtk.GetPointData().GetArray(field_name) )
+
+####### VTK functions ##################################################################################################
+
+
+# Hardcoded data paths and parameters
+path_data = '/media/andrea/data/post_doc_verona/banet/data_set_inference_preop+displ'
+path_to_save = 'sessions/banet_data/dataset'
+
+vts_filename = 'voxelized_displacement.vts'
+nb_channels_in = 4
+
+# vts_filename = 'voxelized.vts'
+# nb_channels_in = 1
+
+nb_channels_out = 1
+nb_points_in_grid = 23328
+input_data_size = (nb_points_in_grid, nb_channels_in)  # 27x27x32x4
+output_data_size = (nb_points_in_grid, nb_channels_out)  # 27x27x32x1
+
+
+# Create directories and arrays
+if not os.path.isdir(path_to_save):
+    os.makedirs(path_to_save)
+input_arr = np.empty((0, input_data_size[0], input_data_size[1]))
+output_arr = np.empty((0, output_data_size[0], output_data_size[1]))
+
+# Read all the sample folders
+for sample in os.listdir(path_data):
+    vts_file = path_data + '/' + sample + '/' + vts_filename
+    # If vts file exists, get inputs and outputs
+    if os.path.exists(vts_file):
+        # Open sample file
+        grid_vtk = readGrid(vts_file)
+        # print(grid_vtk.GetPointData())
+        # Store input numpy arrays
+        if nb_channels_in == 1:
+            input = getDataArray(grid_vtk, 'intraoperativeSurface0').reshape((nb_points_in_grid, 1))
+        elif nb_channels_in == 4:
+            u = getDataArray(grid_vtk, 'displacement')
+            sdf = getDataArray(grid_vtk, 'preoperativeSurface').reshape((nb_points_in_grid, 1))
+            input = np.concatenate((u, sdf), axis=1)
+        # print("Input data shape is {}.".format(input.shape))
+        input_arr = np.concatenate((input_arr, np.array([input])))
+        # Store output numpy arrays
+        output = getDataArray(grid_vtk, 'stiffness').reshape((nb_points_in_grid, 1))
+        # print("Output data shape is {}.".format(output.shape))
+        output_arr = np.concatenate((output_arr, np.array([output])))
+
+# Save numpy arrays
+np.save(path_to_save + '/' + vts_filename[:-4] + '_training_IN_0.npy', input_arr)
+np.save(path_to_save + '/' + vts_filename[:-4] + '_training_OUT_0.npy', output_arr)
+# print("Input data shape is {}.".format(input.shape))
+# print("Output data shape is {}.".format(output.shape))
+print("There are {} samples in total.".format(input_arr.shape[0]))
+
+
